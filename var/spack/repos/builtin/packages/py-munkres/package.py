e3b08fb08a8b97eac60bb059b21c9f782c6d7cf2
Arseny V. Povolotsky <arseny.povolotsky@epfl.ch>
2019-05-27 11:31:23 +0200

    Using Python2-compatible preferred version for 'py-munkres'

--- var/spack/repos/builtin/packages/py-munkres/package.py
+++ var/spack/repos/builtin/packages/py-munkres/package.py
@@ -12,7 +12,7 @@ class PyMunkres(PythonPackage):
     homepage = "https://github.com/bmc/munkres"
     url = "https://pypi.io/packages/source/m/munkres/munkres-1.0.11.tar.gz"
 
-    version('1.1.2', sha256='81e9ced40c3d0ffc48be4b6da5cfdfaa49041faaaba8075b159974ec47926aea', preferred=True)
-    version('1.0.11', sha256='7188f2ed6e8d3eb6c5ec4919200faa6194732b99707b058f7e9f068c588f7eca')
+    version('1.1.2', sha256='81e9ced40c3d0ffc48be4b6da5cfdfaa49041faaaba8075b159974ec47926aea')
+    version('1.0.11', sha256='7188f2ed6e8d3eb6c5ec4919200faa6194732b99707b058f7e9f068c588f7eca', preferred=True)
 
     depends_on('py-setuptools', type='build')
