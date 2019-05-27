0ffbca7f4e7c4953d8f5fcf9cee4cff31e6e7841
Arseny V. Povolotsky <arseny.povolotsky@epfl.ch>
2019-05-27 10:27:07 +0200

    Pin 'py-munkres' version for Python<3.5

--- var/spack/repos/builtin/packages/py-tmd/package.py
+++ var/spack/repos/builtin/packages/py-tmd/package.py
@@ -19,7 +19,8 @@ class PyTmd(PythonPackage):
 
     depends_on('py-enum34@1.0:', type='run', when='^python@:3.3.99')
     depends_on('py-h5py@2.8:', type='run')
-    depends_on('py-munkres@1.0.12:', type='run')
+    depends_on('py-munkres@1.0.11', type='run', when='^python@:3.4.99')
+    depends_on('py-munkres@1.0.12:', type='run', when='^python@3.5:')
     depends_on('py-numpy@1.8:', type='run')
     depends_on('py-scikit-learn@0.19:', type='run')
     depends_on('py-scipy@0.13:', type='run')
