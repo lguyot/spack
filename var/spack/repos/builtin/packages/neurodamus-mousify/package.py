6fe520e203c1692e9cc3cf4d0eb3d55317e7bb56
Ioannis Magkanaris <iomagkanaris@gmail.com>
2019-09-12 14:50:07 +0200

    Changed version formating based on the deployment update

--- var/spack/repos/builtin/packages/neurodamus-mousify/package.py
+++ var/spack/repos/builtin/packages/neurodamus-mousify/package.py
@@ -12,6 +12,7 @@ class NeurodamusMousify(NeurodamusModel):
     git      = "ssh://bbpcode.epfl.ch/sim/models/mousify"
 
     version('develop', git=git, branch='master', submodules=True, clean=False)
-    version('0.2.1', git=git, tag='0.2', submodules=True, clean=False)
+    # Updated CoreNeuron to 0.15 and neurodamus-core to 2.6.0
+    version('0.2-1', git=git, tag='0.2', submodules=True, clean=False)
     version('0.2', git=git, tag='0.2', submodules=True, clean=False)
     version('0.1', git=git, tag='0.1', submodules=True, clean=False)
