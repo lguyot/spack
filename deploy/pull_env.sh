d5abdb1224a42f0676ffcc7513d5c388894bbe48
Matthias Wolf <matthias.wolf@epfl.ch>
2019-10-02 11:46:21 +0200

    Add script to emulate PR environments.

---
+++ deploy/pull_env.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+DEPLOYMENT_ROOT="/gpfs/bbp.cscs.ch/apps/hpc/jenkins"
+
+usage() {
+    echo "$0 pulls/PR STAGE"
+}
+
+pr=$1
+stage=$2
+
+[ -z "$pr" ] && usage
+[ -z "$stage" ] && usage
+
+tmpdir=$(mktemp -d spack_${pr}_XXXXXX)
+spack=$(readlink -f "${DEPLOYMENT_ROOT}/${pr}/spack")
+deployment=$(readlink -f "${DEPLOYMENT_ROOT}/${pr}/deploy/${stage}/latest")
+
+mkdir -p ${tmpdir}/install
+
+cp -Rp ${deployment}/data/{.spack,*} ${tmpdir}
+cp -Rp ${deployment}/.spack-db ${tmpdir}/install
+
+cat <<EOF
+export SOFTS_DIR_PATH=${tmpdir}/install;
+export MODS_DIR_PATH=${tmpdir}/modules;
+export HOME=${tmpdir};
+alias spacktivate="source ${spack}/share/spack/setup-env.sh";
+echo "use the command 'spacktivate' to source the spack of the PR";
+EOF
