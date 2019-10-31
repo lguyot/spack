17ebafc7abe219fc1387c0628c2f5036a2e39301
Fernando Pereira <fernando.pereira@epfl.ch>
2019-10-31 14:48:50 +0100

    Adding mvdtool 2.2.1

--- var/spack/repos/builtin/packages/mvdtool/package.py
+++ var/spack/repos/builtin/packages/mvdtool/package.py
@@ -16,6 +16,7 @@ class Mvdtool(CMakePackage):
     git      = "file:////gpfs/bbp.cscs.ch/home/leite/dev/MVDTool"
 
     version('develop', clean=False, submodules=True)
+    version('2.2.1', tag='v2.2.1', clean=False)
     version('2.2.0', tag='v2.2.0', clean=False)
     version('2.1.0', tag='v2.1.0', clean=False)
     version('2.0.0', tag='v2.0.0', clean=False)
