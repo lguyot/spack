f951a759ab7e9ba945428ba27d64c739d57a4b79
Jorge Blanco Alonso <jblanco@cbdhcp-2-003.epfl.ch>
2019-12-18 14:49:49 +0100

    Set SONATA reports CoreNeuron and Neurodamus branches as default

--- var/spack/repos/builtin/packages/coreneuron/package.py
+++ var/spack/repos/builtin/packages/coreneuron/package.py
@@ -36,7 +36,7 @@ class Coreneuron(CMakePackage):
     homepage = "https://github.com/BlueBrain/CoreNeuron"
     url      = "https://github.com/BlueBrain/CoreNeuron"
 
-    version('sonata_reports', git=url, branch='reporting_api_changes', submodules=True)
+    version('sonata_reports', git=url, branch='reporting_api_changes', preferred=True, submodules=True)
     version('develop', git=url, branch='master', submodules=True)
     version('0.16', git=url, tag='0.16', submodules=True)
     version('0.15', git=url, tag='0.15', submodules=True)
