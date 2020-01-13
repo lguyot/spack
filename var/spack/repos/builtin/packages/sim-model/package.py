2d55679363ac69094a216e5b343e21782c8a9cb9
Nicolas Cornu <nicolas.cornu@epfl.ch>
2020-01-13 11:31:15 +0100

    fix fix

--- var/spack/repos/builtin/packages/sim-model/package.py
+++ var/spack/repos/builtin/packages/sim-model/package.py
@@ -123,10 +123,7 @@ class SimModel(Package):
 
         if self.spec.satisfies('+coreneuron'):
             with working_dir('build_' + mech_name):
-                if self.spec.satisfies('^coreneuron@0.14:0.16.99'):
-                    which('nrnivmech_install.sh', path=".")(prefix)
-                elif self.spec.satisfies('^coreneuron@0.17:'):
-                    which('nrnivmodl-core')("-d", prefix)  # Set dest to install
+                which('nrnivmodl-core')("-d", prefix)  # Set dest to install
 
         # Install special
         shutil.copy(join_path(arch, 'special'), prefix.bin)
