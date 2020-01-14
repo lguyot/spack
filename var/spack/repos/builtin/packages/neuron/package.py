8740919f86dd674cc82ea2401f671da8b3b995c9
Ioannis Magkanaris <iomagkanaris@gmail.com>
2020-01-14 14:57:49 +0100

    Fixes in versions
    - Reverted back explicitly setting the preferred version otherwise 2018-10 is used
    - Set version 7.8.1 to point to the fix for load balance (with the cmake commit) [subjected to change]

--- var/spack/repos/builtin/packages/neuron/package.py
+++ var/spack/repos/builtin/packages/neuron/package.py
@@ -29,7 +29,7 @@ class Neuron(CMakePackage):
     patch('apply_79a4d2af_load_balance_fix.patch', when='@7.8.0b')
 
     version('develop', branch='master')
-    version('7.8.c',   commit='79a4d2a')
+    version('7.8.1',   commit='79a4d2a', preferred=True)
     version('7.8.0b',  commit='92a208b')
     version('7.6.8',   tag='7.6.8')
     version('7.6.6',   tag='7.6.6')
