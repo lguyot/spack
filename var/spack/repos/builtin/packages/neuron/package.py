51bb24ab54a20864ba5bcb120a2e3cf9e3b53250
Ioannis Magkanaris <iomagkanaris@gmail.com>
2020-01-22 11:51:08 +0100

    Bumped testing version to 7.8.2 to avoid reusing old archive in the CI
    - Also added clean=False to version 7.8.0b which is based in a commit

--- var/spack/repos/builtin/packages/neuron/package.py
+++ var/spack/repos/builtin/packages/neuron/package.py
@@ -29,8 +29,8 @@ class Neuron(CMakePackage):
     patch('apply_79a4d2af_load_balance_fix.patch', when='@7.8.0b')
 
     version('develop', branch='master')
-    version('7.8.1',   commit='79a4d2a', clean=False, preferred=True)
-    version('7.8.0b',  commit='92a208b')
+    version('7.8.2',   commit='79a4d2a', clean=False, preferred=True)
+    version('7.8.0b',  commit='92a208b', clean=False)
     version('7.6.8',   tag='7.6.8')
     version('7.6.6',   tag='7.6.6')
     version('2018-10', commit='b3097b7')
