10ab39eccc0d5222de4951a68069d0d025231c17
Blanco Alonso Jorge <jblanco@bb-fvfw811rj1wv.epfl.ch>
2020-01-23 15:41:13 +0100

    Using sonata_reports on develop version of neurodamus-core

--- var/spack/repos/builtin/packages/neurodamus-core/package.py
+++ var/spack/repos/builtin/packages/neurodamus-core/package.py
@@ -17,7 +17,7 @@ class NeurodamusCore(SimModel):
     git      = "ssh://bbpcode.epfl.ch/sim/neurodamus-core"
 
     version('sonata_reports', branch='sandbox/jblanco/sonata_reports', preferred=True, get_full_repo=False)
-    version('develop', branch='master', get_full_repo=False)
+    version('develop', branch='sandbox/jblanco/sonata_reports', get_full_repo=False)
     version('2.9.2', tag='2.9.2', get_full_repo=False)
     version('2.9.1', tag='2.9.1', get_full_repo=False)
     version('2.9.0', tag='2.9.0', get_full_repo=False)
