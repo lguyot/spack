c44ac75834f9547d889a9cc8d42c243e5734b2c2
Pramod Kumbhar <pramod.s.kumbhar@gmail.com>
2020-03-30 23:22:24 +0200

    Bump py-zmq to be able to re-build py-zmq

--- var/spack/repos/builtin/packages/py-zmq/package.py
+++ var/spack/repos/builtin/packages/py-zmq/package.py
@@ -11,6 +11,7 @@ class PyZmq(PythonPackage):
     homepage = "https://github.com/zeromq/pyzmq"
     url      = "https://github.com/zeromq/pyzmq/archive/v14.7.0.tar.gz"
 
+    version('17.1.3', sha256='49dc9d3c38d803f436784bcdddd35354b946d37d0a777a301bd493655348b773')
     version('17.1.2', sha256='77a32350440e321466b1748e6063b34a8a73768b62cb674e7d799fbc654b7c45')
     version('16.0.2', sha256='717dd902c3cf432b1c68e7b299ad028b0de0d0a823858e440b81d5f1baa2b1c1')
     version('14.7.0', sha256='809a5fcc720d286c840f7f64696e60322b5b2544795a73db626f09b344d16a15')
