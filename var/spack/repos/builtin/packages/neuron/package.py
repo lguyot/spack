8d7cd38446f2af893258f37b3e6e4a9e21e5a979
Ioannis Magkanaris <iomagkanaris@gmail.com>
2020-04-17 12:10:38 +0200

    Updated to latest commit

--- var/spack/repos/builtin/packages/neuron/package.py
+++ var/spack/repos/builtin/packages/neuron/package.py
@@ -30,7 +30,7 @@ class Neuron(CMakePackage):
     patch("apply_79a4d2af_load_balance_fix.patch", when="@7.8.0b")
 
     version("develop", branch="master")
-    version("7.8.0c",  commit="5cd5acd", preferred=True)
+    version("7.8.0c",  commit="81f9e60", preferred=True)
     version("7.8.0b",  commit="92a208b")
     version("7.6.8",   tag="7.6.8")
     version("7.6.6",   tag="7.6.6")
