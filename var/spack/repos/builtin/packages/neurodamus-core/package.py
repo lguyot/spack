dfb00ca89f323df6f58025d466c9e30a16dc2ea8
Blanco Alonso Jorge <jblanco@bbpv1.epfl.ch>
2020-04-24 11:36:25 +0200

    Fix neurodamus-core to use sonata_reports branch

--- var/spack/repos/builtin/packages/neurodamus-core/package.py
+++ var/spack/repos/builtin/packages/neurodamus-core/package.py
@@ -19,7 +19,7 @@ class NeurodamusCore(SimModel):
     homepage = "ssh://bbpcode.epfl.ch/sim/neurodamus-core"
     git      = "ssh://bbpcode.epfl.ch/sim/neurodamus-core"
 
-    version('develop', branch='master', get_full_repo=False)
+    version('develop', branch='sandbox/jblanco/sonata_reports', get_full_repo=False)
     version('sonata_reports', branch='sandbox/jblanco/sonata_reports', get_full_repo=False)
     version('2.10.0', tag='2.10.0', get_full_repo=False)
     version('2.9.3', tag='2.9.3', get_full_repo=False)
