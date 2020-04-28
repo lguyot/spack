c3c877308d711d1f818af8d50028f01a53b5d864
Ioannis Magkanaris <iomagkanaris@gmail.com>
2020-04-28 19:06:37 +0200

    Fix autocond-archive url

--- var/spack/repos/builtin/packages/autoconf-archive/package.py
+++ var/spack/repos/builtin/packages/autoconf-archive/package.py
@@ -9,6 +9,6 @@ class AutoconfArchive(AutotoolsPackage):
     GNU Autoconf."""
 
     homepage = "https://www.gnu.org/software/autoconf-archive/"
-    url = "http://mirror.us-midwest-1.nexcess.net/gnu/autoconf-archive/autoconf-archive-2019.01.06.tar.xz"
+    url = "http://ftpmirror.gnu.org/autoconf-archive/autoconf-archive-2019.01.06.tar.xz"
 
     version('2019.01.06', sha256='17195c833098da79de5778ee90948f4c5d90ed1a0cf8391b4ab348e2ec511e3f')
